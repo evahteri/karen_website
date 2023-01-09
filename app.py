a169dad1c38175547259ea8a34cf8c881934d9c5
Eero Vahteri <eero.vahteri@helsinki.fi>
2023-01-09 19:10:45 +0200

    gallery view, example pics, styling

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    return render_template("index.html")
+    return render_template("gallery.html")
 
 @app.route("/info")
 def info():
 
     return render_template("info.html")
+
+@app.route("/gallery")
+def gallery():
+
+    return render_template("gallery.html")
